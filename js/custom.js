6b0d62d256fc6540cbe979dce2c30086a4842764
tuongvi0 <tuongvi0391@gmail.com>
2024-09-17 21:23:16 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function(){
+		$(".btn-toggle").click(function(){
+			$("#menu").slideToggle()
+		})
+		
+		$("#btn-dropdown").click(function(){
+			$("#dropdown-menu").slideToggle()
+		})
+		
+	})
+	
